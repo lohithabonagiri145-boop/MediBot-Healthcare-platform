b5f1270c64a9d4fea44572f627065d29a0e89255
lohithabonagiri145-boop <lohithabonagiri145@gmail.com>
2026-05-09 14:03:48 +0530

    Add files via upload

---
+++ chatbot.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+  (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '69e8ba5f781ce3df51c5c686' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          voice: {
+            url: "https://runtime-api.voiceflow.com"
+          }
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+  })(document, 'script');
+</script>
